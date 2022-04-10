e9433d78bff7b568263af018a06c7cf5565db4d4
정승원 <seungwon@jeongseung-won-ui-MacBookPro.local>
2022-04-10 13:13:05 +0900

    9_revisees

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,6 +59,24 @@ function blog_posts() {
         post_html.push(post_template);
     }
 
+    // for the more posts link
+    let post_template = `
+    <div class="blog-post" onclick="blog_link_click('https://blog.naver.com/redlion0929');">
+
+        <div class="blog-link">
+
+            <h3><a href="https://blog.naver.com/redlion0929">Naver Blog</a></h3>            
+
+        </div>
+
+        <div class="blog-goto-link">
+            <img class="blog-arrow" src="/assets/images/right-open-mini.svg"/>
+        </div>
+    </div>
+    `;
+
+    post_html.push(post_template);
+
     $('#rss-feeds').html(post_html);
 
 }

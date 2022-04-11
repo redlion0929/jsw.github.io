aaa8e302b21d8811a6107cd5e752a72865b3d36e
정승원 <seungwon@jeongseung-won-ui-MacBookPro.local>
2022-04-11 15:50:25 +0900

    11_revisess

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,6 +52,40 @@ function blog_posts() {
 
     post_html.push(post_template);
 
+    let post_template2 = `
+    <div class="blog-post" onclick="blog_link_click('https://mercurial-shingle-b81.notion.site/Paper-Study-75bba119d4be4b5db5667ddd786b67e9');">
+
+        <div class="blog-link">
+
+            <h3><a href="https://mercurial-shingle-b81.notion.site/Paper-Study-75bba119d4be4b5db5667ddd786b67e9">NLP Paper Study</a></h3>            
+
+        </div>
+
+        <div class="blog-goto-link">
+            <img class="blog-arrow" src="/assets/images/right-open-mini.svg"/>
+        </div>
+    </div>
+    `;
+
+    post_html.push(post_template2);
+
+    let post_template3 = `
+    <div class="blog-post" onclick="blog_link_click('https://mercurial-shingle-b81.notion.site/2433f5ea414a4ea7a1079d5a0618a84a');">
+
+        <div class="blog-link">
+
+            <h3><a href="https://mercurial-shingle-b81.notion.site/2433f5ea414a4ea7a1079d5a0618a84a">DeepLearning Study</a></h3>            
+
+        </div>
+
+        <div class="blog-goto-link">
+            <img class="blog-arrow" src="/assets/images/right-open-mini.svg"/>
+        </div>
+    </div>
+    `;
+
+    post_html.push(post_template3);
+
     $('#rss-feeds').html(post_html);
 
 }
